0264348a97079201fb604dd065b6bd3967ec2298
SergioManzano10 <152727584+SergioManzano10@users.noreply.github.com>
2024-01-24 11:24:58 +0100

    Add files via upload

---
+++ Eighth App (Web Scraping)/Web_Scraping.py
@@ -0,0 +1,67 @@
+from importlib.resources import contents
+import requests
+import selectorlib 
+import smtplib, ssl
+import time
+
+URL = "http://programmer100.pythonanywhere.com/tours/" 
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'} # It is needed to display the webpage´s info
+
+
+def scrape(url):
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source): 
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yml")
+    value = extractor.extract(source)["tours"] 
+    return value
+
+
+def send_email(message):
+    host = "smtp.gmail.com" 
+    port = 465
+
+    username = "smanzano250800@gmail.com" 
+    password = "razl xzkv ggfz dqbw"
+
+    receiver = "smanzano250800@gmail.com" 
+    context = ssl.create_default_context() 
+
+    with smtplib.SMTP_SSL(host, port, context = context) as server: 
+        server.login(username, password) 
+        server.sendmail(username, receiver, message) 
+
+
+message = f"""\
+Subject: "New date on the tour, buy the tickets!
+
+Go to the website to reserve them as soon as possible
+"""
+
+def store(extracted):
+    with open("data.txt", "a") as file: 
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted) 
+                send_email(message)
+
+    time.sleep(2) 
---
+++ Eighth App (Web Scraping)/Web_Scraping_SQL.py
@@ -0,0 +1,77 @@
+from importlib.resources import contents
+import requests
+import selectorlib 
+import smtplib, ssl
+import time
+import sqlite3
+
+URL = "http://programmer100.pythonanywhere.com/tours/" 
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'} # It is needed to display the webpage´s info
+
+
+connection = sqlite3.connect("new_data.db") 
+
+
+def scrape(url):
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source): 
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yml")
+    value = extractor.extract(source)["tours"] 
+    return value
+
+
+def send_email(message):
+    host = "smtp.gmail.com" 
+    port = 465
+
+    username = "smanzano250800@gmail.com" 
+    password = "razl xzkv ggfz dqbw"
+
+    receiver = "smanzano250800@gmail.com" 
+    context = ssl.create_default_context() 
+
+    with smtplib.SMTP_SSL(host, port, context = context) as server: 
+        server.login(username, password) 
+        server.sendmail(username, receiver, message) 
+
+
+message = f"""\
+Subject: "New date on the tour, buy the tickets!
+
+Go to the website to book them as soon as possible
+"""
+
+def store(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row) 
+    connection.commit() 
+
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row 
+    cursor = connection.cursor() 
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date)) 
+    rows = cursor.fetchall()
+    return rows
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        
+        if extracted != "No upcoming tours":
+            row = read(extracted) 
+            if not row: 
+                store(extracted) 
+                send_email(message)
+
+    time.sleep(2) 
