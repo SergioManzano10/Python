cacad1702fc6aa9fb06f0fcfa89d999a643933dd
SergioManzano10 <152727584+SergioManzano10@users.noreply.github.com>
2024-02-23 19:51:22 +0100

    Add files via upload

---
+++ Eleventh App (Chatbot)/backend.py
@@ -0,0 +1,20 @@
+import openai
+
+class Chatbot:
+    def __init__(self):
+        openai.api_key = "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" # api_key se establece a nivel del módulo openai, lo que lo hace global para todas las instancias y otras partes del código que importan openai
+
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine = "text-davinci-edit-001", # Esto no funciona
+            prompt = user_input,
+            max_tokens = 3000, 
+            temperature = 0.5
+        ).choices[0].text
+        return response
+    
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    response = chatbot.get_response("Write a joke about birds.")
+    print(response)
