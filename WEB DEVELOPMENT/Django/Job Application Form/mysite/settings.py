d91cb965d04b9e9a16801cf8cab866681fcdda96
SERGIO MANZANO SÁNCHEZ <152727584+SergioManzano10@users.noreply.github.com>
2024-09-01 12:46:39 +0200

    Update settings.py

--- WEB DEVELOPMENT/Django/Job Application Form/mysite/settings.py
+++ WEB DEVELOPMENT/Django/Job Application Form/mysite/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "smanzano250800@gmail.com"
-EMAIL_HOST_PASSWORD = "kbhl ouce fqjd iyyp"
+EMAIL_HOST_USER = "yourgmail@gmail.com"
+EMAIL_HOST_PASSWORD = "yourpassword"
